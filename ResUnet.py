971d55e17350a28bbda85f3844678748ceb027c8
mako_chan <50488995+mtakamat@users.noreply.github.com>
2019-11-10 23:56:21 +0900

    ResUnet

---
+++ ResUnet.py
@@ -0,0 +1,112 @@
+#define generator model
+from keras.models import *
+from keras.layers import Input, Conv2D, UpSampling2D, BatchNormalization, Activation, add, concatenate
+from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Conv2DTranspose
+
+def res_block(x, nb_filters, strides):
+    res_path = BatchNormalization()(x)
+    res_path = Activation(activation='relu')(res_path)
+    res_path = Conv2D(filters=nb_filters[0], kernel_size=(3, 3), padding='same', strides=strides[0])(res_path)
+    res_path = BatchNormalization()(res_path)
+    res_path = Activation(activation='relu')(res_path)
+    res_path = Conv2D(filters=nb_filters[1], kernel_size=(3, 3), padding='same', strides=strides[1])(res_path)
+
+    shortcut = Conv2D(nb_filters[1], kernel_size=(1, 1), strides=strides[0])(x)
+    shortcut = BatchNormalization()(shortcut)
+
+    res_path = add([shortcut, res_path])
+    return res_path
+def encoder(x):
+    to_decoder = []
+
+    main_path = Conv2D(filters=16, kernel_size=(3, 3), padding='same', strides=(1, 1))(x)
+    main_path = BatchNormalization()(main_path)
+    main_path = Activation(activation='relu')(main_path)
+
+    main_path = Conv2D(filters=16, kernel_size=(3, 3), padding='same', strides=(1, 1))(main_path)
+
+    shortcut = Conv2D(filters=16, kernel_size=(1, 1), strides=(1, 1))(x)
+    shortcut = BatchNormalization()(shortcut)
+
+    main_path = add([shortcut, main_path])
+    # first branching to decoder
+    to_decoder.append(main_path)
+    main_path = res_block(main_path, [32, 32], [(2, 2), (1, 1)])
+    to_decoder.append(main_path)
+
+    main_path = res_block(main_path, [64, 64], [(2, 2), (1, 1)])
+    to_decoder.append(main_path)
+
+    main_path = res_block(main_path, [128, 128], [(2, 2), (1, 1)])
+    to_decoder.append(main_path)
+
+    main_path = res_block(main_path, [256, 256], [(2, 2), (1, 1)])
+    to_decoder.append(main_path)
+
+    main_path = res_block(main_path, [512, 512], [(2, 2), (1, 1)])
+    to_decoder.append(main_path)
+
+    return to_decoder
+
+def decoder(x, from_encoder):
+    main_path = UpSampling2D(size=(2, 2))(x)
+    main_path = concatenate([main_path, from_encoder[5]], axis=3)
+    main_path = res_block(main_path, [512, 512], [(1, 1), (1, 1)])
+
+    main_path = UpSampling2D(size=(2, 2))(main_path)
+    main_path = concatenate([main_path, from_encoder[4]], axis=3)
+    main_path = res_block(main_path, [256, 256], [(1, 1), (1, 1)])
+
+    main_path = UpSampling2D(size=(2, 2))(main_path)
+    main_path = concatenate([main_path, from_encoder[3]], axis=3)
+    main_path = res_block(main_path, [128, 128], [(1, 1), (1, 1)])
+
+    main_path = UpSampling2D(size=(2, 2))(main_path)
+    main_path = concatenate([main_path, from_encoder[2]], axis=3)
+    main_path = res_block(main_path, [64, 64], [(1, 1), (1, 1)])
+
+    main_path = UpSampling2D(size=(2, 2))(main_path)
+    main_path = concatenate([main_path, from_encoder[1]], axis=3)
+    main_path = res_block(main_path, [32, 32], [(1, 1), (1, 1)])
+
+    main_path = UpSampling2D(size=(2, 2))(main_path)
+    main_path = concatenate([main_path, from_encoder[0]], axis=3)
+    main_path = res_block(main_path, [16, 16], [(1, 1), (1, 1)])
+
+    return main_path
+
+# define the generator model
+def define_generator(image_shape=(256,256,3)):
+	# weight initialization
+	init = RandomNormal(stddev=0.02)
+	# image input
+	inputs = Input(shape=image_shape)
+
+	to_decoder = encoder(inputs)
+
+	path = res_block(to_decoder[5], [1024, 1024], [(2, 2), (1, 1)])
+	path = decoder(path, from_encoder=to_decoder)
+
+	path = Conv2D(filters=1, kernel_size=(1, 1), activation='sigmoid')(path)
+
+	outputs = Activation('tanh')(path)
+
+	model = Model(input=inputs, output=outputs)
+	# summarize model
+	model.summary()
+	return model
+
+""
+# define image shape
+image_shape = (256,256,3)
+# create the model
+model = define_generator(image_shape)
+# summarize the model
+model.summary()
+# plot the model
+plot_model(model, to_file='generator_model_plot.png', show_shapes=True, show_layer_names=True)
+
+""
